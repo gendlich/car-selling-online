a4be641f0a481e4d913462a7276b4bc58b0fc9c6
Guilherme Endlich <g.endlich.g@gmail.com>
2022-10-05 16:01:31 -0300

    WEB: Rotas do admin estao protegidas

--- car-selling-online-web/src/routes/index.tsx
+++ car-selling-online-web/src/routes/index.tsx
@@ -1,13 +1,21 @@
 import {
     Routes,
-    Route
+    Route,
+    Navigate,
 } from "react-router-dom";
+import { useAuth } from "../authContext";
 import CarroDetalhes from "../pages/carroDetalhes";
 import Home from "../pages/home";
 import Login from '../pages/login';
 import NotFound from '../pages/notFound';
 import PainelAdmin from "../pages/painelAdmin";
 import NovoCarro from "../pages/painelAdmin/novoCarro";
+
+const RequireAuth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const { authenticated } = useAuth();
+
+    return authenticated ? <>{children}</> : <Navigate to='/login' replace />;
+}
 
 export function Router() {
     return (
@@ -18,9 +26,10 @@ export function Router() {
             <Route path='*' element={<NotFound />} />
 
             {/* rotas privadas */}
-            <Route path='/painel' element={<PainelAdmin />} />
-            <Route path='/carro/:id/editar' element={<NovoCarro />} />
-            <Route path='/carro/novo' element={<NovoCarro />} />
+            <Route path='/painel' element={<RequireAuth><PainelAdmin /></RequireAuth>} />
+        
+            <Route path='/carro/:id/editar' element={<RequireAuth><NovoCarro /></RequireAuth>} />
+            <Route path='/carro/novo' element={<RequireAuth><NovoCarro /></RequireAuth>} />
         </Routes>
     )
 }
